837f1938b87a507ba5f3f702a369ae11efc0dfdb
Victor Poglioni <victorrenop16@gmail.com>
2020-09-30 21:42:42 -0300

    Assignment 5: gutter game and all ones game

--- assignments/assignment_5/kata_bowling.py
+++ assignments/assignment_5/kata_bowling.py
@@ -1,9 +1,9 @@
 class KataBowling:
     def __init__(self):
-        self.match_score = []
+        self.match_score = 0
 
     def roll(self, round_score: int) -> int:
-        pass
+        self.match_score += round_score
 
     def score(self) -> int:
-        pass
+        return self.match_score
--- tests/unit/assignments/assignment_5/test_kata_bowling.py
+++ tests/unit/assignments/assignment_5/test_kata_bowling.py
@@ -1,9 +1,15 @@
 from assignments.assignment_5.kata_bowling import KataBowling
 
+game = KataBowling()
+
 class TestKataBowling:
     def test_gutter_game(self):
-        game = KataBowling()
         for i in range(20):
             game.roll(0)
         assert game.score() == 0
+
+    def test_all_ones(self):
+        for i in range(20):
+            game.roll(1)
+        assert game.score() == 20
         
