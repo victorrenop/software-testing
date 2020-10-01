773ab7319c29208a0e9bf0008ea60318971c819d
Victor Poglioni <victorrenop16@gmail.com>
2020-09-30 22:28:53 -0300

    Assignment 5: Refactor to bonus calculation

--- assignments/assignment_5/kata_bowling.py
+++ assignments/assignment_5/kata_bowling.py
@@ -12,23 +12,32 @@ class KataBowling:
         frame_index = 0
         for frame in range(10):
             if self._is_strike(frame_index):
-                score += 10 + self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
+                score += 10 + self._strike_bonus(frame_index)
                 frame_index += 1
             elif self._is_spare(frame_index):
-                score += 10 + self.rolls[frame_index + 2]
+                score += 10 + self._spare_bonus(frame_index)
                 frame_index += 2
             else:
-                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                score += self._sum_in_frame(frame_index)
                 frame_index += 2
 
         return score
 
-    def _is_spare(self, idx):
+    def _is_strike(self, idx: int) -> bool:
+        if self.rolls[idx] == 10:
+            return True
+        return False
+
+    def _strike_bonus(self, idx: int) -> int:
+        return self.rolls[idx + 1] + self.rolls[idx + 2]
+
+    def _is_spare(self, idx: int) -> bool:
         if self.rolls[idx] + self.rolls[idx + 1] == 10:
             return True
         return False
 
-    def _is_strike(self, idx):
-        if self.rolls[idx] == 10:
-            return True
-        return False
+    def _spare_bonus(self, idx: int) -> int:
+        return self.rolls[idx + 2]
+    
+    def _sum_in_frame(self, idx: int) -> int:
+        return self.rolls[idx] + self.rolls[idx + 1]
