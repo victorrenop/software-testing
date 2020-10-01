5b9c2691dd13d4829136fd647d78a9c53598e84c
Victor Poglioni <victorrenop16@gmail.com>
2020-09-30 22:31:28 -0300

    Assignment 5: perfect game test

--- tests/unit/assignments/assignment_5/test_kata_bowling.py
+++ tests/unit/assignments/assignment_5/test_kata_bowling.py
@@ -23,7 +23,14 @@ class TestKataBowling:
         game.roll(10)
         game.roll(3)
         game.roll(4)
+        self.roll_many(16, 0, game)
         assert game.score() == 24
+
+    def test_perfect_game(self):
+        game = KataBowling()
+        self.roll_many(12, 10, game)
+        assert game.score() == 300
+
 
     def roll_many(self, rolls: int, pins: int, game: KataBowling):
         for i in range(rolls):
