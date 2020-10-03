fbabc0139b925fa5d15adef769444a4063efa404
Victor Poglioni <victorrenop16@gmail.com>
2020-10-02 21:18:06 -0300

    Assignment 6: added feature to calc the first interval of damage

--- assignments/assignment_6/damage_classifier.py
+++ assignments/assignment_6/damage_classifier.py
@@ -5,4 +5,21 @@ class RPGDamageClassifier:
         self.third_multiplier = 0.15
 
     def damage_dealt(self, damage: float, damage_type: str) -> float:
-        return 0.0
+        total_damage_dealt = 0
+        if damage_type == 'physical':
+            total_damage_dealt = self._calc_physical_damage(damage)
+        elif damage_type == 'magical':
+            total_damage_dealt = self._calc_magical_damage(damage)
+        return total_damage_dealt
+    
+    def _calc_physical_damage(self, damage: float) -> float:
+        total_damage_dealt = damage
+        if damage >= 1000.0 and damage <= 3000.0:
+            total_damage_dealt += total_damage_dealt*self.first_multiplier
+        return float("{:.2f}".format(total_damage_dealt))
+
+    def _calc_magical_damage(self, damage: float) -> float:
+        total_damage_dealt = damage
+        if damage >= 1000.0 and damage <= 3000.0:
+            total_damage_dealt += total_damage_dealt*self.first_multiplier
+        return float("{:.2f}".format(total_damage_dealt))
