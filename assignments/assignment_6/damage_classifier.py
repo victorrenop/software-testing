1d5d8335c840c7413d118b66af3c7e51f86a0da3
Victor Poglioni <victorrenop16@gmail.com>
2020-10-02 22:23:44 -0300

    Assignment 6: lint changes

--- assignments/assignment_6/damage_classifier.py
+++ assignments/assignment_6/damage_classifier.py
@@ -6,26 +6,26 @@ class RPGDamageClassifier:
 
     def damage_dealt(self, damage: float, damage_type: str) -> float:
         total_damage_dealt = 0
-        if damage_type == 'physical':
+        if damage_type == "physical":
             total_damage_dealt = self._calc_physical_damage(damage)
-        elif damage_type == 'magical':
+        elif damage_type == "magical":
             total_damage_dealt = self._calc_magical_damage(damage)
         return total_damage_dealt
-    
+
     def _calc_physical_damage(self, damage: float) -> float:
         bonus_damage = 0
         if damage >= 1000.0:
-            bonus_damage += (min(damage - 1000.00, 999.99))*self.first_multiplier
+            bonus_damage += (min(damage - 1000.00, 999.99)) * self.first_multiplier
         if damage >= 3000.0:
-            bonus_damage += min(damage - 3000.00, 1999.99)*self.second_multiplier
+            bonus_damage += min(damage - 3000.00, 1999.99) * self.second_multiplier
         if damage >= 5000.0:
-            bonus_damage += (damage - 5000.00)*self.third_multiplier
+            bonus_damage += (damage - 5000.00) * self.third_multiplier
         return float("{:.2f}".format(damage + bonus_damage))
 
     def _calc_magical_damage(self, damage: float) -> float:
         bonus_damage = 0
         if damage >= 1000.0:
-            bonus_damage += min(damage - 1000.0, 999.99)*self.first_multiplier
+            bonus_damage += min(damage - 1000.0, 999.99) * self.first_multiplier
         if damage >= 5000.0:
-            bonus_damage += (damage - 5000.00)*self.third_multiplier
+            bonus_damage += (damage - 5000.00) * self.third_multiplier
         return float("{:.2f}".format(damage + bonus_damage))
